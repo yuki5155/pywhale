88bc389510bfc268249b2f871643a3c63c2a6fcf
Yuki Asano <asanoyuuki@asanoyuukinoMacBook-Pro.local>
2022-07-17 17:04:16 +0900

    update

--- setup.py
+++ setup.py
@@ -9,9 +9,18 @@ setup(
     author_email = 'yukiasano@example.com',
     description = 'Hoge',
     install_requires = ['setuptools'],
-    packages=['pywhale.templates.python_temps'],
-    package_dir={'pywhale.templates.python_temps': 'pywhale/templates'},
-    package_data={'pywhale.templates.python_temps': ['python_temps/*']},
+    packages=[
+        "pywhale"
+        'pywhale.templates.python_temps'
+    ],
+    package_dir={
+        'pywhale.templates.python_temps': 'pywhale/templates'
+        "pywhale"
+    },
+    package_data={
+        "pywhale": ['./*'],
+        'pywhale.templates.python_temps': ['python_temps/*']
+    },
     entry_points = {
         'console_scripts': [
             'pywhale = pywhale.core.managers:start_command',
