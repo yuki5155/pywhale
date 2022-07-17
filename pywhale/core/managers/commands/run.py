88a02f7f83e4092049f57a09b9e84785ebc53707
Yuki Asano <asanoyuuki@asanoyuukinoMacBook-Pro.local>
2022-07-17 17:50:58 +0900

    update

--- pywhale/core/managers/commands/run.py
+++ pywhale/core/managers/commands/run.py
@@ -8,7 +8,7 @@ class RunPythonClass(BaseClass):
         dir = os.getcwd()
         with open(dir + "/projectparams.pkl", "rb") as p:
             param = pickle.load(p)
-        
+        cmd = self.parser.parse_args.cmd
         # print(param.workdir)
         # ディレクトリ元を丸ごとコピーしてコンテナ内に入れる
 
@@ -19,5 +19,5 @@ class RunPythonClass(BaseClass):
         )
         # 指定されたコマンドを実行
         self.run_docker_command(
-            f"docker exec --workdir /app/src/{param.workdir} pywhale python aaa.py"
+            f"docker exec --workdir /app/src/{param.workdir} pywhale python {cmd}"
         )
