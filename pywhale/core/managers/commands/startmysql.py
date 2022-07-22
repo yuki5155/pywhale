d592a958ab0eb04b2a34cc444edcdbdeccfd2095
Yuki Asano <asanoyuuki@asanoyuukinoMacBook-Pro.local>
2022-07-23 00:52:02 +0900

    update

--- pywhale/core/managers/commands/startmysql.py
+++ pywhale/core/managers/commands/startmysql.py
@@ -8,5 +8,16 @@ class StartMySQLClass(BaseClass):
         a = [container.name for container in self.client.containers.list()]
         print(a)
         if not "pywhalemysql" in a:
-            self.client.containers.run('mysql', ports={'3306/tcp': ('0.0.0.0', 3306)})
+            self.client.containers.run(
+                'mysql', 
+                ports={
+                    '3306/tcp': ('0.0.0.0', 3306)
+                },
+                environment = {
+                    "MYSQL_ROOT_PASSWORD": "sample",
+                    "MYSQL_USER": "sample",
+                    "MYSQL_PASSWORD": "sample"
+                }
+            )
+
 
