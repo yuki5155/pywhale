f201f3731e76a3684d2439ed1e2d131c7a406a1a
Yuki Asano <asanoyuuki@asanoyuukinoMacBook-Pro.local>
2022-07-23 02:44:46 +0900

    update

--- pywhale/core/managers/base.py
+++ pywhale/core/managers/base.py
@@ -2,14 +2,21 @@ import argparse
 import subprocess
 import docker
 
+
 class BaseClass:
     parser = argparse.ArgumentParser(
-        description='Process some integers.', 
+        description='Process some integers.',
         formatter_class=argparse.RawTextHelpFormatter
     )
     command = subprocess
     client = docker.from_env()
+
     def run_docker_command(self, cmd=None):
         cmd = cmd.split(" ")
-        image_list = subprocess.run(cmd, stdout=subprocess.PIPE, text=True, check=True)
+        image_list = subprocess.run(
+            cmd,
+            stdout=subprocess.PIPE,
+            text=True,
+            check=True
+        )
         print(image_list.stdout)
--- pywhale/core/managers/commands/startmysql.py
+++ pywhale/core/managers/commands/startmysql.py
@@ -26,5 +26,18 @@ class StartMySQLClass(BaseClass):
             except docker.errors.APIError:
                 # まずは消す
                 self.client.containers.prune()
+                self.client.containers.run(
+                    'mysql', 
+                    ports={
+                        '3306/tcp': ('0.0.0.0', 3306)
+                    },
+                    environment = {
+                        "MYSQL_ROOT_PASSWORD": "sample",
+                        "MYSQL_USER": "sample",
+                        "MYSQL_PASSWORD": "sample"
+                    },
+                    detach=True,
+                    name="pywhalemysql"
+                )
 
 
