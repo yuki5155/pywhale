a37f3292dc2cad8d7b700f0bff9ddd8cfbc361c9
Yuki Asano <asanoyuuki@asanoyuukinoMacBook-Pro.local>
2022-07-23 17:33:37 +0900

    update

--- pywhale/core/managers/commands/run.py
+++ pywhale/core/managers/commands/run.py
@@ -2,19 +2,20 @@ from pywhale.core.managers.base import BaseClass
 import pickle
 import os
 
+
 class RunPythonClass(BaseClass):
     def run_command(self):
-        #parameterを読み込む
+        # parameterを読み込む
         dir = os.getcwd()
         with open(dir + "/projectparams.pkl", "rb") as p:
             param = pickle.load(p)
         cmd = self.parser.parse_args().cmd
 
-        nocache = self.parser.parse_args().nocache
+        # nocache = self.parser.parse_args().nocache
         # print(param.workdir)
+
         # ディレクトリ元を丸ごとコピーしてコンテナ内に入れる
 
-        # print(f"docker cp .{param.workdir} pywhale:/app/src")
         # docker内のworkdir内にコピーする
         self.run_docker_command(
             f"docker cp .{param.workdir} pywhale:/app/src"
