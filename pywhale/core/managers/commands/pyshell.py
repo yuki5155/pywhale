f04e12dc05972184e22c92ff2e7721afeee6efb4
Yuki Asano <asanoyuuki@asanoyuukinoMacBook-Pro.local>
2022-07-29 01:30:05 +0900

    update

--- pywhale/core/managers/commands/pyshell.py
+++ pywhale/core/managers/commands/pyshell.py
@@ -8,5 +8,8 @@ class PyShellClass(BaseClass):
     def run_command(self):
         args, unknown = self.parser.parse_known_args()
         print(args, unknown)
+        if args.t == None:
+            a = [container.name for container in self.client.containers.list()]
+            print(a)
         # os.system(f'docker container exec -it pywhale bash')
         # os.system('docker container exec -it pywhale bash')
