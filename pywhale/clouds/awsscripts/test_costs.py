ae779c45aabcd39b7c85a291c1d1b8f0fba55927
Yuki Asano <yukiasano55@gmail.com>
2022-12-04 23:31:59 +0900

    fix test

--- pywhale/clouds/awsscripts/test_costs.py
+++ pywhale/clouds/awsscripts/test_costs.py
@@ -1,9 +1,8 @@
 from costs_base import AwsCost
 
 def test_aws_sample():
-    print("Hello")
+   
     aws_cost = AwsCost()
-    print(aws_cost.get_current_cost())
     try:
         current_cost = float(aws_cost.get_current_cost())
         assert type(current_cost) == float
